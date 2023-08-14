91dce8542c6147053e1a0a620872078a6b6171b2
riverpadilla <riverpadilla@msn.com>
2023-08-13 18:04:18 -0600

    Se corrige error "v-on is not bound" en archivo manager.html

---
+++ src/main/resources/static/web/js/cards.js
@@ -0,0 +1,37 @@
+Vue.createApp({
+    data() {
+        return {
+            clientInfo: {},
+            creditCards: [],
+            debitCards: [],
+            errorToats: null,
+            errorMsg: null,
+        }
+    },
+    methods: {
+        getData: function () {
+            urlParams = new URLSearchParams(window.location.search);
+            id = urlParams.get('id');
+            if(id == null) id=1;
+            axios.get(`/api/clients/${id}`)
+            //axios.get("/api/clients/1")
+                .then((response) => {
+                    //get client ifo
+                    this.clientInfo = response.data;
+                    this.creditCards = this.clientInfo.cards.filter(card => card.type == "CREDIT");
+                    this.debitCards = this.clientInfo.cards.filter(card => card.type == "DEBIT");
+                })
+                .catch((error) => {
+                    this.errorMsg = "Error getting data";
+                    this.errorToats.show();
+                })
+        },
+        formatDate: function (date) {
+            return new Date(date).toLocaleDateString('en-gb');
+        }
+    },
+    mounted: function () {
+        this.errorToats = new bootstrap.Toast(document.getElementById('danger-toast'));
+        this.getData();
+    }
+}).mount('#app')
